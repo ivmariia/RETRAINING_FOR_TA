30370de21fe506fa2c8d5a5283de89b7ca2446af
Mariia Ivanova <84564851+ivmariia@users.noreply.github.com>
2024-05-25 20:17:34 +0300

    Added Homework1

---
+++ Homework_1/script.js
@@ -0,0 +1,78 @@
+// TASK_2
+// console.log("Mariia Ivanova");
+// END_TASK_2
+
+// TASK_3
+// let fruitName = "Orange";
+// let vegetableName = "Potato";
+// alert(`The value of variable fruitName is ${fruitName}\r\nThe value of variable vegetableName is ${vegetableName}`);
+// fruitName = vegetableName;
+// alert(`The new value of variable fruitName is ${fruitName}\r\nThe value of variable vegetableName is ${vegetableName}`);
+// END_TASK_3
+
+// TASK_4
+// let fruit = {
+//     name:"Orange",
+//     weight: 0.200,
+//     isAlergic: true,
+//     originCountry: undefined,
+//     isOrganic: null 
+// };
+// END_TASK_4
+
+// TASK_5
+// let isAdult = confirm("Are you 18 years old?");
+// console.log(isAdult);
+// END_TASK_5
+
+// TASK_6
+// let firstName = "Mariia";
+// let lastName = "Ivanova";
+// let studyGroupName = "Retraining for TA";
+// let yearOfBirth = 1996;
+// let isMarried = true;
+// console.log(`yearofBirth: ${yearOfBirth}`);
+// console.log(`isMarried: ${isMarried}`);
+// console.log(`firstName: ${firstName}`);
+// console.log(`lastName: ${lastName}`);
+// console.log(`studyGroupName: ${studyGroupName}`);
+
+// let gradeForHomeWork = undefined;
+// let projectName = null;
+// console.log("Name: gradeForHomework\r\nType: " + typeof(gradeForHomeWork));
+// console.log("Name: projectName\r\nType: " + typeof(projectName));
+// END_TASK_6
+
+// TASK_7
+// let loginName = prompt("Please enter your Login name:");
+// let email = prompt("Please enter your E-mail Adress:");
+// let password = prompt("Please enter your password:");
+
+// alert(`Dear ${loginName}, your e-mail is ${email}, your password is ${password}`);
+// END_TASK_7
+
+// TASK_8
+// const SECONDS_IN_MINUTE = 60;
+// const MINUTES_IN_HOUR = 60;
+// const HOURS_IN_DAY = 24;
+
+// let secondsInHour = SECONDS_IN_MINUTE * MINUTES_IN_HOUR;
+// let secondsInDay = secondsInHour * HOURS_IN_DAY;
+
+// const DAYS_IN_FEBRUARY_NOT_LEAP_YEAR = 28;
+// const DAYS_IN_FEBRUARY_LEAP_YEAR = 29;
+// const DAYS_IN_SHORT_MONTH = 30;
+// const DAYS_IN_LONG_MONTH = 31;
+
+// let secondsInFebNotLeapYear = secondsInDay * DAYS_IN_FEBRUARY_NOT_LEAP_YEAR;
+// let secondsInFebLeapYear = secondsInDay * DAYS_IN_FEBRUARY_LEAP_YEAR;
+// let secondsInShortMonth = secondsInDay * DAYS_IN_SHORT_MONTH;
+// let secondsInLongMonth = secondsInDay * DAYS_IN_LONG_MONTH;
+
+// alert(`There are ${secondsInHour} seconds in an hour.  
+//       \r\nThere are ${secondsInDay} seconds in a day.
+//       \r\nThere are ${secondsInFebLeapYear} seconds in February on a leap year.
+//       \r\nThere are ${secondsInFebNotLeapYear} seconds in February on not a leap year.
+//       \r\nThere are ${secondsInShortMonth} seconds in a short month.
+//       \r\nThere are ${secondsInLongMonth} seconds in a long month.`);
+// END_TASK_8
